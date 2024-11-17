f0e2af68576866a0d97d3169587d124de0555d19
darkfos82 <darkfos82@gmail.com>
2024-11-17 19:50:21 +0300

    add: redux in project

--- converter/src/index.js
+++ converter/src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import storeRedux from './store/storeRedux';
 import App from './App';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-          <Route element={<App />} path='/'></Route>
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+  <Provider store={storeRedux}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <Routes>
+            <Route element={<App />} path='/'></Route>
+        </Routes>
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>
 );
